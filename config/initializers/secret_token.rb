11690137af5102955312622618c468c6aed3530d
unknown <sa@.appsys.appsys.net>
2010-09-03 16:47:34 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StorageApp::Application.config.secret_token = 'b13454e5d9d9e38d723255011e65778aac615498853d20fa7f3d4cc3654acd19755fcc7de206274e620d422d772918869a19fc61e150971bc58f7b7cd4feb386'
